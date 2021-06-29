81f9e07373cb541b85a8c60d393f948a0110fd54
Ryan Safaeian <ryansafa2@gmail.com>
2021-06-29 14:28:11 -0500

    added one-to-many associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
- validates :tittle, presence: true, length: {minimum:6, maximum: 100}
- validates :description, presence: true,  length: {minimum:10, maximum: 300}
+   belongs_to :user
+  validates :tittle, presence: true, length: {minimum:6, maximum: 100}
+  validates :description, presence: true,  length: {minimum:10, maximum: 300}
 end
