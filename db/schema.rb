eb0c0b77a2b03de8456ff91a939dd7520dd4223c
Ryan Safaeian <ryansafa2@gmail.com>
2021-07-02 16:50:27 -0500

    Final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_234910) do
+ActiveRecord::Schema.define(version: 2021_07_02_194848) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "tittle"
@@ -18,6 +25,12 @@ ActiveRecord::Schema.define(version: 2021_07_01_234910) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
